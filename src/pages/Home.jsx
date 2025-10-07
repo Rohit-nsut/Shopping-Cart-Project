7b6155104a5cd803dd30e11d2a6230119be1d88d
Roht <rohittondak38@gmail.com>
2025-10-07 22:12:42 +0530

    New

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Product from '../components/Product';
 import Spinner from "../components/Spinner";
+import { FaInstagram, FaTwitter, FaFacebook } from "react-icons/fa";
 
 export default function Home() {
   const API_URL = "https://fakestoreapi.com/products";
@@ -60,7 +61,6 @@ export default function Home() {
 
   return (
     <div className="w-screen min-h-screen bg-white pb-12 flex flex-col">
-      {/* Main Content */}
       <div className="max-w-6xl xl:mx-auto mx-4 my-8 flex-1">
         <div className="text-center py-10">
           <h1 className="text-5xl font-black mb-4 text-center text-gray-900 tracking-wide drop-shadow-lg">
@@ -104,7 +104,7 @@ export default function Home() {
             <button className="bg-green-600 text-white px-8 py-3 rounded-xl font-bold shadow-lg hover:bg-green-700 hover:scale-105 transition-all">View All Products</button>
           </a>
         </div>
-        {/* Modal for product details */}
+        
         {modalProduct && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
             <div
@@ -126,7 +126,7 @@ export default function Home() {
           </div>
         )}
       </div>
-      {/* Footer Section */}
+      
       <footer className="w-full mt-16">
         <div className="bg-black py-8 px-4 text-white">
           <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-6">
@@ -136,13 +136,13 @@ export default function Home() {
             </div>
             <div className="flex gap-6 items-center">
               <a href="#" className="hover:text-green-400" title="Instagram">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-7 h-7"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 1.366.062 2.633.334 3.608 1.308.975.974 1.246 2.242 1.308 3.608.058 1.266.07 1.646.07 4.85s-.012 3.584-.07 4.85c-.062 1.366-.334 2.633-1.308 3.608-.974.975-2.242 1.246-3.608 1.308-1.266.058-1.646.07-4.85.07s-3.584-.012-4.85-.07c-1.366-.062-2.633-.334-3.608-1.308-.975-.974-1.246-2.242-1.308-3.608C2.175 15.647 2.163 15.267 2.163 12s.012-3.584.07-4.85c.062-1.366.334-2.633 1.308-3.608C4.516 2.497 5.784 2.226 7.15 2.163 8.416 2.105 8.796 2.163 12 2.163zm0-2.163C8.741 0 8.332.013 7.052.072 5.771.131 4.659.396 3.678 1.378c-.981.981-1.247 2.093-1.306 3.374C2.013 5.668 2 6.077 2 12c0 5.923.013 6.332.072 7.612.059 1.281.325 2.393 1.306 3.374.981.981 2.093 1.247 3.374 1.306C8.332 23.987 8.741 24 12 24s3.668-.013 4.948-.072c1.281-.059 2.393-.325 3.374-1.306.981-.981 1.247-2.093 1.306-3.374.059-1.28.072-1.689.072-7.612 0-5.923-.013-6.332-.072-7.612-.059-1.281-.325-2.393-1.306-3.374-.981-.981-2.093-1.247-3.374-1.306C15.668.013 15.259 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zm0 10.162a3.999 3.999 0 1 1 0-7.998 3.999 3.999 0 0 1 0 7.998zm6.406-11.845a1.44 1.44 0 1 0 0 2.88 1.44 1.44 0 0 0 0-2.88z"/></svg>
+                <FaInstagram className="w-7 h-7" />
               </a>
               <a href="#" className="hover:text-green-400" title="Twitter">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-7 h-7"><path d="M24 4.557a9.93 9.93 0 0 1-2.828.775 4.932 4.932 0 0 0 2.165-2.724c-.951.555-2.005.959-3.127 1.184A4.916 4.916 0 0 0 16.616 3c-2.717 0-4.92 2.206-4.92 4.917 0 .386.044.762.127 1.124C7.691 8.816 4.066 6.864 1.64 3.94c-.423.722-.666 1.561-.666 2.475 0 1.708.87 3.216 2.188 4.099a4.904 4.904 0 0 1-2.229-.616c-.054 1.997 1.397 3.872 3.448 4.292a4.936 4.936 0 0 1-2.224.084c.627 1.956 2.444 3.377 4.6 3.417A9.867 9.867 0 0 1 0 19.54a13.94 13.94 0 0 0 7.548 2.212c9.058 0 14.009-7.513 14.009-14.009 0-.213-.005-.425-.014-.636A10.025 10.025 0 0 0 24 4.557z"/></svg>
+                <FaTwitter className="w-7 h-7" />
               </a>
               <a href="#" className="hover:text-green-400" title="Facebook">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-7 h-7"><path d="M22.675 0h-21.35C.595 0 0 .592 0 1.326v21.348C0 23.408.595 24 1.326 24h11.495v-9.294H9.691v-3.622h3.13V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.797.143v3.24l-1.918.001c-1.504 0-1.797.715-1.797 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116c.73 0 1.324-.592 1.324-1.326V1.326C24 .592 23.408 0 22.675 0"/></svg>
+                <FaFacebook className="w-7 h-7" />
               </a>
             </div>
           </div>
